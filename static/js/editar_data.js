e0d12690c3cbefe4156b59b8715ad41c578c1e9c
AmairaniMartinezM <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-12-10 23:53:14 -0600

    Update editar_data.js

--- static/js/editar_data.js
+++ static/js/editar_data.js
@@ -3,7 +3,7 @@ function editar_data(email){
     var email = urlParams.get('email');
     console.log("email: " + email);
     // const URL = "http://localhost:8000/contactos";
-    const URL = "https://python-back-3b9ec03f9254.herokuapp.com/contactos"
+    const URL = "https://python-back-3b9ec03f9254.herokuapp.com//contactos"
     var request = new XMLHttpRequest;
     request.open('GET',URL +"/" +email,true);
 
