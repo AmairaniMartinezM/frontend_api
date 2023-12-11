96edae109808dee23bdbefc6e7268c5643594909
AmairaniMartinezM <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-12-10 23:54:44 -0600

    Update insert.js

--- static/js/insert.js
+++ static/js/insert.js
@@ -1,5 +1,5 @@
 function insert(email, nombre, telefono){
-    const URL = "https://python-back-3b9ec03f9254.herokuapp.com/contactos"
+    const URL = "https://python-back-3b9ec03f9254.herokuapp.com//contactos"
     //const URL = "http://localhost:8000/contactos";
     var request = new XMLHttpRequest;
     request.open('POST',URL)
